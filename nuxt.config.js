1f59e4b448b6c7c348f0f3773c8764e60532d19b
Titanio Yudsita <titaniy98@gmail.com>
2022-09-23 17:51:12 +0700

    deploy to netlify

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'serverless',
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
